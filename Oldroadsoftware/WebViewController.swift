5450592a66bd1d42b9ee907644770c10f58ffe69
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-17 07:36:01 +0530

    add activity indicator

--- Oldroadsoftware/WebViewController.swift
+++ Oldroadsoftware/WebViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 import WebKit
 
 class WebViewController: UIViewController {
-
+    
     @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let myURL = URL(string:"http://atrkapp-cicd.atrk.net/qrredirect.aspx?label_number=M2HFJXVUM8&ok=1&latitude=48.8328516&longitude=2.7269344&type=html")
+               let myRequest = URLRequest(url: myURL!)
+               webView.load(myRequest)
     }
-    }
+}
