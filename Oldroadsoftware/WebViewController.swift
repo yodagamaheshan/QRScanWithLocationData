2f805bb0deb741abfb383e890332800783b2c513
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-17 07:45:22 +0530

    add webview delegate

--- Oldroadsoftware/WebViewController.swift
+++ Oldroadsoftware/WebViewController.swift
@@ -17,8 +17,14 @@ class WebViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
         let myURL = URL(string:"http://atrkapp-cicd.atrk.net/qrredirect.aspx?label_number=M2HFJXVUM8&ok=1&latitude=48.8328516&longitude=2.7269344&type=html")
                let myRequest = URLRequest(url: myURL!)
                webView.load(myRequest)
+    }
+}
+extension WebViewController: WKNavigationDelegate{
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        indicator.stopAnimating()
     }
 }
