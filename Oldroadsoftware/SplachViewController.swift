62c235c23ea0fa6a1235997a6f5a7175afce40a2
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-17 07:21:16 +0530

    add QR scanner

--- Oldroadsoftware/SplachViewController.swift
+++ Oldroadsoftware/SplachViewController.swift
@@ -7,24 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
+import QRCodeReader
 
 class SplachViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    lazy var readerVC: QRCodeReaderViewController = {
+        let builder = QRCodeReaderViewControllerBuilder {
+            $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+            
+            // Configure the view controller (optional)
+            $0.showTorchButton        = false
+            $0.showSwitchCameraButton = false
+            $0.showCancelButton       = true
+            $0.showOverlayView        = true
+            $0.rectOfInterest         = CGRect(x: 0.2, y: 0.2, width: 0.6, height: 0.6)
+        }
+        
+        return QRCodeReaderViewController(builder: builder)
+    }()
 
-    /*
-    // MARK: - Navigation
+    @IBAction func scanAction(_ sender: Any) {
+        // Retrieve the QRCode content
+         // By using the delegate pattern
+         readerVC.delegate = self
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+         // Or by using the closure pattern
+         readerVC.completionBlock = { (result: QRCodeReaderResult?) in
+           print(result)
+         }
+
+         // Presents the readerVC as modal form sheet
+         readerVC.modalPresentationStyle = .formSheet
+        
+         present(readerVC, animated: true, completion: nil)
     }
-    */
+    
+}
 
+extension SplachViewController: QRCodeReaderViewControllerDelegate{
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+        reader.stopScanning()
+
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+        
+    }
+    
+    
 }
