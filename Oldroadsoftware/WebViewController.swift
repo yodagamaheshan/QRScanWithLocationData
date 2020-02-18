6a0b03157be001f2f6fa229c88a7af4b3953ef09
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-18 10:31:18 +0530

    add getting URL method by component

--- Oldroadsoftware/WebViewController.swift
+++ Oldroadsoftware/WebViewController.swift
@@ -12,6 +12,10 @@ import WebKit
 class WebViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
+    var lat: String?
+    var long: String?
+    var baseURL: String?
+    var labelNumber: String?
     
     @IBOutlet weak var indicator: UIActivityIndicatorView!
     
@@ -21,6 +25,20 @@ class WebViewController: UIViewController {
         let myURL = URL(string:"http://atrkapp-cicd.atrk.net/qrredirect.aspx?label_number=M2HFJXVUM8&ok=1&latitude=48.8328516&longitude=2.7269344&type=html")
                let myRequest = URLRequest(url: myURL!)
                webView.load(myRequest)
+    }
+    
+    func getURL(baseUrlString: String,with labelNumber:String, latiyude lat: String,and lon: String) -> URL?{
+        let pathOne = "qrredirect.aspx"
+        let labelNumber = URLQueryItem(name: "label_number" , value: labelNumber)
+        let ok = URLQueryItem(name: "ok", value: "1")
+        let lat = URLQueryItem(name: "latitude", value: lat)
+        let lon = URLQueryItem(name: "longitude", value: lon)
+        let type = URLQueryItem(name: "type", value: "html")
+        
+        var urlComponnent = URLComponents(string: baseUrlString)
+        urlComponnent?.path = pathOne
+        urlComponnent?.queryItems = [labelNumber,ok,lat,lon,type]
+        return urlComponnent?.url
     }
 }
 extension WebViewController: WKNavigationDelegate{
