3ad9c7010818dd7ba5be621ecab85bd16e6d1cbe
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-18 11:16:44 +0530

    bug fixes with getUrl method

--- Oldroadsoftware/WebViewController.swift
+++ Oldroadsoftware/WebViewController.swift
@@ -13,7 +13,7 @@ class WebViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
     var lat: String?
-    var long: String?
+    var lon: String?
     var baseURL: String?
     var labelNumber: String?
     
@@ -22,12 +22,17 @@ class WebViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         webView.navigationDelegate = self
-        let myURL = URL(string:"http://atrkapp-cicd.atrk.net/qrredirect.aspx?label_number=M2HFJXVUM8&ok=1&latitude=48.8328516&longitude=2.7269344&type=html")
-               let myRequest = URLRequest(url: myURL!)
-               webView.load(myRequest)
+        //http://atrkapp-cicd.atrk.net/qrredirect.aspx?label_number=M2HFJXVUM8&ok=1&latitude=48.8328516&longitude=2.7269344&type=html
+        if lat != nil, lon != nil, baseURL != nil, labelNumber != nil,
+            let myURL = getURL(baseUrlStringWithScheme: baseURL!, with: labelNumber!, latiyude: lat!, and: lon!){
+            let myRequest = URLRequest(url: myURL)
+            webView.load(myRequest)
+        }
+        
     }
     
-    func getURL(baseUrlString: String,with labelNumber:String, latiyude lat: String,and lon: String) -> URL?{
+    func getURL(baseUrlStringWithScheme: String,with labelNumber:String, latiyude lat: String,and lon: String) -> URL?{
+        let schemeAndBaseUrl = URL(string: baseUrlStringWithScheme)
         let pathOne = "qrredirect.aspx"
         let labelNumber = URLQueryItem(name: "label_number" , value: labelNumber)
         let ok = URLQueryItem(name: "ok", value: "1")
@@ -35,10 +40,10 @@ class WebViewController: UIViewController {
         let lon = URLQueryItem(name: "longitude", value: lon)
         let type = URLQueryItem(name: "type", value: "html")
         
-        var urlComponnent = URLComponents(string: baseUrlString)
-        urlComponnent?.path = pathOne
-        urlComponnent?.queryItems = [labelNumber,ok,lat,lon,type]
-        return urlComponnent?.url
+        var urlComponnent = URLComponents()
+        urlComponnent.path = pathOne
+        urlComponnent.queryItems = [labelNumber,ok,lat,lon,type]
+        return urlComponnent.url(relativeTo: schemeAndBaseUrl)
     }
 }
 extension WebViewController: WKNavigationDelegate{
